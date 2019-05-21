ee3b7b47831b8bf7ec39f0ec950c4fae0a8bd60b
wouter <wouter173@outlook.com>
2019-05-21 20:45:38 +0200

    files 🌟

---
+++ index.py
@@ -0,0 +1,17 @@
+from reducer import Reduce
+import deco
+
+#startup
+
+deco.clear()
+print(deco.center(deco.color(' fraction reducer ', "green"), "="))
+
+#test values
+Reduce("5/7")
+Reduce("4/6")
+Reduce("11/10")
+Reduce("8/4")
+Reduce("7/4")
+Reduce("6/4")
+Reduce("300/200")
+Reduce("8/4")
---
+++ reducer.py
@@ -0,0 +1,54 @@
+import deco
+import math
+
+def Reduce(inp):
+  inp = inp.split("/")
+  if len(inp) != 2:
+    return print("invalid input")
+  
+  numerator = int(inp[0])
+  denominator = int(inp[1])
+
+  common_Divisor = Differance(Primes(numerator), Primes(denominator))
+  numerator = numerator / common_Divisor
+  denominator = denominator / common_Divisor
+  denominator, numerator = int(denominator), int(numerator)
+
+  if denominator == 1:
+    print(deco.color("{} => {}".format("/".join(inp), numerator), "green"))
+  else:
+    print(deco.color("{} => {}/{}".format("/".join(inp), numerator, denominator), "green"))
+
+def Primes(number): 
+  out_list = []
+  i = 2
+  
+  while number > 1:
+    if number % i == 0:
+      out_list.append(i)
+      number = number / i
+      i -= 1
+
+    i += 1
+
+  return out_list
+
+def Differance(list_one, list_two):
+
+  finish = []
+
+  for index_one, number_one in enumerate(list_one):
+    for index_two, number_two in enumerate(list_two):
+      if number_one == number_two:
+        finish.append(number_one)
+        del list_two[index_two]
+
+  multiplied = 0
+  for number in finish:
+    if multiplied == 0:
+      multiplied = number
+    else:
+      multiplied = multiplied * number
+
+  return(multiplied)
+
